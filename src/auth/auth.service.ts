19790b4edfb05006d167b6631f2d4bb7e1eb6f4a
Pablo Perez <perez51160900@hotmail.com>
2021-09-14 08:40:50 -0400

    adding signin y signup

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { UsersRepository } from './repositories/users.repository';
+import * as bcrypt from 'bcrypt'
+import { JwtService } from '@nestjs/jwt';
+
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @InjectRepository(UsersRepository)
+        private usersRepository: UsersRepository,
+        private jwtService: JwtService,
+    ) {}
+
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void>{
+        return this.usersRepository.createUser(authCredentialsDto);
+    }
+    
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string>{
+        const { username, password } = authCredentialsDto; 
+        const user = await this.usersRepository.findOne({ username });
+
+        if(user && (await bcrypt.compare(password, user.password))){
+            return 'success'
+        }
+        throw new UnauthorizedException('Please check your login credentials');
+    }
+}
