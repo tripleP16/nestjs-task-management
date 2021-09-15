80619eee1598bcec213b4715c4a26bc642865f00
Pablo Perez <perez51160900@hotmail.com>
2021-09-15 12:42:46 -0400

    configuration files done

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,42 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 @Module({
   imports: [
-    TasksModule, 
-    TypeOrmModule.forRoot({
-      type:'postgres', 
-      host:'localhost', 
-      port:5432, 
-      username:'postgres', 
-      password:'12345678', 
-      database:'task-management', 
-      autoLoadEntities:true, 
-      synchronize: true
-    }), AuthModule,
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.dev`],
+      validationSchema: configValidationSchema,
+    }),
+    TasksModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USER'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+        };
+      },
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: '12345678',
+    //   database: 'task-management',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
+    AuthModule,
   ],
   controllers: [],
   providers: [],
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,17 +6,29 @@ import { UsersRepository } from './repositories/users.repository';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule,
     TypeOrmModule.forFeature([UsersRepository]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: 'topSecret51',
-      signOptions: {
-        expiresIn: 3600,
-      },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: 3600,
+        },
+      }),
     }),
+    // JwtModule.register({
+    //   secret: 'topSecret51',
+    //   signOptions: {
+    //     expiresIn: 3600,
+    //   },
+    // }),
   ],
   providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
